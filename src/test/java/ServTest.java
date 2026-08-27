8cccbe11e9c49d7821672b1cfcb0caf13d2d16cb
egordoga <edoga@ukr.net>
2026-08-27 11:34:50 +0300

    changed GitHub webhook

--- src/test/java/ServTest.java
+++ src/test/java/ServTest.java
@@ -8,4 +8,9 @@ class ServTest {
         int sum = new Serv().sum(1, 2);
         Assertions.assertEquals(3, sum);
     }
+
+    @Test
+    void test() {
+        System.out.println("Build started from GitHub webhook!");
+    }
 }
